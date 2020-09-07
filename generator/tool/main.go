aa22bd84607eeb0206bc8323585b7af568173c62
Travis Lane <coderlane@gmail.com>
2020-09-06 21:35:34 -0400

    Add a sample parser tool
    This parses text files and outputs the stringified version. It helps to
    identify parsing failures.

---
+++ generator/tool/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/coderlane/go-minecraft-rcon/generator"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		log.Fatal("Specify a file to parse.")
+	}
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		out, err := generator.ParseCommandWithAlias(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(out.String())
+	}
+}
